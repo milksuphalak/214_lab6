6649bc364e9813b3c0658dab379c945ec9d8c166
milksuphalak <suphalak.l@ku.th>
2021-03-18 16:46:43 +0700

    1

--- 1.c
+++ 1.c
@@ -2,23 +2,30 @@
 #include <string.h>
 int main ()
 {
-    int n,i;
+    int n,i,j;
     scanf ("%d",&n);
-    char sort[n][100];
+    char sort[n][1000],s[1000];
     for (i=0;i<n;i++)
     {
         scanf ("%s",sort[i]);
     }
+    for(i=0;i<n;i++)
     {
-    for (j=i+1;j<n;j++)
+        for (j=i+1;j<n;j++)
         {
             if (strcmp(sort[i],sort[j]) > 0)
             {
-               strcpy(s,sort[i]);
+                strcpy(s,sort[i]);
                 strcpy(sort[i],sort[j]);
                 strcpy(sort[j],s);
 
             }
         }
     }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",sort[i]);
+    }
+return 0;
 }
