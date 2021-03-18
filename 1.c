88495550cadda0c3c11e092f4a27a28e7fccdbe3
milksuphalak <suphalak.l@ku.th>
2021-03-18 16:36:12 +0700

    Update 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    int n,i;
+    scanf ("%d",&n);
+    char sort[n][100];
+    for (i=0;i<n;i++)
+    {
+        scanf ("%s",sort[i]);
+
+
+    }
+       for (i=0;i<n;i++)
+    {
+
+
+    }
+}
