184506b74ba27b31aed3d9cf4aa2ce68c9bffa9a
milksuphalak <suphalak.l@ku.th>
2021-03-18 16:37:46 +0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -11,9 +11,13 @@ int main ()
 
 
     }
-       for (i=0;i<n;i++)
     {
+    for (j=i+1;j<n;j++)
+        {
+            if (strcmp(sort[i],sort[j]) > 0)
+            {
 
-
+            }
+        }
     }
 }
