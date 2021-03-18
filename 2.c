372d02d7b1fc235216c2449840170f511b16213c
milksuphalak <suphalak.l@ku.th>
2021-03-18 16:55:00 +0700

    2

--- 2.c
+++ 2.c
@@ -4,9 +4,17 @@ int main()
     int n,i,m=0;
     scanf ("%d",&n);
     char x[2][n];
-    for (i=0;i<n;i++)
+    for (i=0;i<2;i++)
     {
         scanf ("%s",x[i]);
     }
-
+ for(i=0;i<n;i++)
+    {
+        if(x[0][i]==x[1][i])
+        {
+            m++;
+        }
+    }
+    printf("%d %d",m,n-m);
+    return 0;
 }
