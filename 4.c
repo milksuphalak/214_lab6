aa82cb63380842ab514e8c8bec833118c6074dd2
milksuphalak <suphalak.l@ku.th>
2021-03-18 17:20:15 +0700

    Update 4.c

--- 4.c
+++ 4.c
@@ -6,6 +6,12 @@ int main()
     int p[n],s[2][n];
     for(i=0;i<n;i++)
     {
-
+        p[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&s[0][i],&s[1][i]);
+        p[s[0][i]]++;
+        p[s[1][i]]++;
     }
 }
